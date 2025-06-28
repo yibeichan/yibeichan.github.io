56ea2fa3d472cb36dfb3f7fdec4838f73ad24b44
Yibei Chen <yibeichan@gmail.com>
2025-06-28 06:49:51 -0400

    Fix SPA routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,20 @@ import Software from './components/Software'
 import Contact from './components/Contact'
 import CV from './components/CV'
 
+// Single Page Apps for GitHub Pages
+// https://github.com/rafgraph/spa-github-pages
+// Check if we're being redirected from a 404 page
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
